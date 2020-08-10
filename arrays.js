a2759f61d89b95db6d2b691a93143806046897be
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2020-08-10 14:57:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array
 }
 function removeElementFromBeginningOfArray(array) {
-array.slice()
+array.slice(1)
 return(array)
 }
 function removeElementFromEndOfArray(array) {
